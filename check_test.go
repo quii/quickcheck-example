e2dc8b740add04d3e3b704a4036076a16556d90d
Chris James <qui666@gmail.com>
2016-03-07 21:20:07 +0000

    reformataroo

--- check_test.go
+++ check_test.go
@@ -5,21 +5,19 @@ import (
 	"testing/quick"
 )
 
-
-func add(x, y int) int{
+func add(x, y int) int {
 	return x + y
 }
 
-func TestAddingEx1(t *testing.T){
-	if add(3, 2) != 5{
+func TestAddingEx1(t *testing.T) {
+	if add(3, 2) != 5 {
 		t.Error("3 plus 2 is 5")
 	}
 }
 
-func TestAddingZeroMakesNoDifference(t *testing.T){
+func TestAddingZeroMakesNoDifference(t *testing.T) {
 	assertion := func(x int) bool {
-		result := add(x, 0)
-		return result == x
+		return add(x, 0) == x
 	}
 
 	if err := quick.Check(assertion, nil); err != nil {
@@ -27,11 +25,9 @@ func TestAddingZeroMakesNoDifference(t *testing.T){
 	}
 }
 
-func TestAssociativity(t *testing.T){
+func TestAssociativity(t *testing.T) {
 	assertion := func(x int) bool {
-		result1 := add(add(x, 1), 1)
-		result2 := add(x, 2)
-		return result1 ==result2
+		return add(add(x, 1), 1) == add(x, 2)
 	}
 
 	if err := quick.Check(assertion, nil); err != nil {
@@ -39,11 +35,9 @@ func TestAssociativity(t *testing.T){
 	}
 }
 
-func TestParamOrderDoesNotMatter(t *testing.T){
+func TestParamOrderDoesNotMatter(t *testing.T) {
 	assertion := func(x, y int) bool {
-		result1 := add(x, y)
-		result2 := add(y, x)
-		return result1==result2
+		return add(x, y) == add(y, x)
 	}
 
 	if err := quick.Check(assertion, nil); err != nil {
